5f8bcdd45cee7797fa788502a22ff1034d6efe26
Wender1903 <wenderman74@gmail.com>
2025-06-16 16:48:19 -0300

    :sparkles: feat: Adicionando conteudos dinamicos, pegando dados do servidor e exibindo esses dados na view do projeto

--- jogoteca.py
+++ jogoteca.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+        
 app = Flask(__name__)
 
 @app.route('/inicio')
 def index():
-    return render_template('lista.html')
+    jogo1 = Jogo('Minecraft', 'SandBox', 'XBOX-360')
+    jogo2 = Jogo('Hotweels World Race', 'Aventura', 'Playstation 2')
+    jogo3 = Jogo('Roblox', 'Criatividade', 'Celular e Computador')
+    lista_jogos = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo = 'Jogos', jogos=lista_jogos)
 app.run(host='0.0.0.0', port=8080)
