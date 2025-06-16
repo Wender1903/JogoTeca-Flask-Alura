d1446adca446ca20b08b1dd335b7f8491efb17fc
Wender1903 <wenderman74@gmail.com>
2025-06-16 17:17:09 -0300

    :sparkles: feat: Criando formulário para criar novo jogo com a rota POST

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,14 +6,28 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-        
+jogo1 = Jogo('Minecraft', 'SandBox', 'XBOX-360')
+jogo2 = Jogo('Hotweels World Race', 'Aventura', 'Playstation 2')
+jogo3 = Jogo('Roblox', 'Criatividade', 'Celular e Computador')
+lista_jogos = [jogo1, jogo2, jogo3]     
+
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 def index():
-    jogo1 = Jogo('Minecraft', 'SandBox', 'XBOX-360')
-    jogo2 = Jogo('Hotweels World Race', 'Aventura', 'Playstation 2')
-    jogo3 = Jogo('Roblox', 'Criatividade', 'Celular e Computador')
-    lista_jogos = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo = 'Jogos', jogos=lista_jogos)
-app.run(host='0.0.0.0', port=8080)
+
+@app.route('/novo')
+def novo_jogo():
+    return render_template('novojogo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST', ])
+def criar_jogo():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+    return render_template('lista.html', titulo='Novo Jogo', jogos=lista_jogos)
+
+app.run(host='0.0.0.0', port=8080, debug=True)
